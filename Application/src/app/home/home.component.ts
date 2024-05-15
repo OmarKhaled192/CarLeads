8a5fd1cfc269d42904f8651edf3ea8aa0cb7dc7e
omarkhaled <okoros192000@gmail.com>
2024-05-15 07:53:34 +0300

    update-app-component

--- Application/src/app/home/home.component.ts
+++ Application/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LeadsInfoService } from '../Services/leads-info.service';
+import { Lead } from '../Interfaces/lead';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public allLeads!: Lead[];
+
+  constructor(private _LeadsInfoService:LeadsInfoService) { }
 
   ngOnInit(): void {
+    this.getAllLeads();
   }
 
+  getAllLeads() {
+    return this._LeadsInfoService.getAllLeads().subscribe({
+      next: (res) => {
+        this.allLeads = res;
+        console.log(this.allLeads); // for test
+      },
+      error: (err) => {
+        console.log(err); // for test
+      }
+    })
+  }
 }
