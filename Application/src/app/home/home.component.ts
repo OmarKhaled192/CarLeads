d05f6c5e260effc8f25a433b0ca15b923ba03a08
omarkhaled <okoros192000@gmail.com>
2024-05-15 08:00:18 +0300

    handeling-errors-of-home-component

--- Application/src/app/home/home.component.ts
+++ Application/src/app/home/home.component.ts
@@ -8,8 +8,8 @@ import { Lead } from '../Interfaces/lead';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
   public allLeads!: Lead[];
+  public error500: boolean = false;
 
   constructor(private _LeadsInfoService:LeadsInfoService) { }
 
@@ -21,11 +21,16 @@ export class HomeComponent implements OnInit {
     return this._LeadsInfoService.getAllLeads().subscribe({
       next: (res) => {
         this.allLeads = res;
-        console.log(this.allLeads); // for test
+        console.log(this.allLeads);
+        this.error500 = false;
       },
       error: (err) => {
-        console.log(err); // for test
+        console.log(err);
+        if (err.status == 500) {
+          this.error500 = true;
+        }  
       }
     })
   }
+
 }
