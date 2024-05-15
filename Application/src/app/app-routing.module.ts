87f4cd9afe9f23d7e771c1c6277613918a79b1d1
omarkhaled <okoros192000@gmail.com>
2024-05-15 09:08:07 +0300

    change-faveicon

--- Application/src/app/app-routing.module.ts
+++ Application/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PotentialsComponent } from './potentials/potentials.component';
 
 const routes: Routes = [
   {path: "", redirectTo: "home", pathMatch:"full" },
   {path: "home", title: "home", component:HomeComponent },
+  {path: "potentialsOf/:lead_id", title: "potentials",component: PotentialsComponent}
 ];
 
 @NgModule({
